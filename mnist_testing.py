3f680b26bcda5c5d60cb08994b9165ab6ba60e3c
JamesT94 <jamest9401@gmail.com>
2021-02-05 16:17:39 +0000

    updates

--- mnist_testing.py
+++ mnist_testing.py
@@ -12,15 +12,15 @@ import h5py
 
 # --- Data setup --- #
 def load_data():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
+    train_dataset = pd.read_csv('datasets/mnist_train.csv')
+    train_set_y_orig = np.array(train_dataset['label'])  # your train set labels
+    train_set_x_orig = np.array(train_dataset.drop('label', axis=1))  # your train set features
 
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+    test_dataset = pd.read_csv('datasets/mnist_test.csv')
+    test_set_y_orig = np.array(test_dataset['label'])  # your train set labels
+    test_set_x_orig = np.array(test_dataset.drop('label', axis=1))  # your train set features
 
-    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+    classes = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])  # the list of classes
 
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
@@ -30,11 +30,6 @@ def load_data():
 
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 
-# Example of a picture
-index = 10
-plt.imshow(train_x_orig[index])
-print("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.")
-
 # Explore your dataset
 m_train = train_x_orig.shape[0]
 num_px = train_x_orig.shape[1]
@@ -42,25 +37,32 @@ m_test = test_x_orig.shape[0]
 
 print("Number of training examples: " + str(m_train))
 print("Number of testing examples: " + str(m_test))
-print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
 print("train_x_orig shape: " + str(train_x_orig.shape))
 print("train_y shape: " + str(train_y.shape))
 print("test_x_orig shape: " + str(test_x_orig.shape))
 print("test_y shape: " + str(test_y.shape))
 
-# Reshape the training and test examples
-train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
-test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+# # Reshape the training and test examples
+# train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
+# test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 
 # Standardize data to have feature values between 0 and 1.
-train_x = train_x_flatten / 255.
-test_x = test_x_flatten / 255.
+train_x = train_x_orig / 255.
+test_x = test_x_orig / 255.
+
+train_x = train_x.T
+test_x = test_x.T
+
+train_x = train_x[:, :1000]
+train_y = train_y[:, :1000]
+test_x = test_x[:, :500]
+test_y = test_y[:, :500]
 
 print("train_x's shape: " + str(train_x.shape))
 print("test_x's shape: " + str(test_x.shape))
 
 # --- Backward Propagation --- #
-layers_dims = [12288, 20, 7, 5, 1]
+layers_dims = [784, 20, 7, 5, 1]
 
 
 def L_layer_model(X, Y, layers_dims, final_activation, learning_rate=0.0075, num_iterations=3000, print_cost=False):
@@ -98,7 +100,6 @@ def L_layer_model(X, Y, layers_dims, final_activation, learning_rate=0.0075, num
     plt.show()
 
     return parameters
-
 
 parameters = L_layer_model(train_x, train_y, layers_dims, 'sigmoid', learning_rate=0.0075,
                            num_iterations=2500, print_cost=True)
