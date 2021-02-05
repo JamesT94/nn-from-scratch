8d448dd045412721b0c2a326388c81fb77fbc403
JamesT94 <jamest9401@gmail.com>
2021-02-05 16:50:15 +0000

    bads

--- helper_functions.py
+++ helper_functions.py
@@ -36,28 +36,17 @@ def relu_back(dA, cache):
     return dZ
 
 
-def softmax(Z):
-    A = np.exp(Z) / np.sum(np.exp(Z), axis=1, keepdims=True)
-    return A
-
-
-def print_mislabeled_images(classes, X, y, p):
-    """
-    Plots images where predictions and truth were different.
-    X -- dataset
-    y -- true labels
-    p -- predictions
-    """
-    a = p + y
-    mislabeled_indices = np.asarray(np.where(a == 1))
-    plt.rcParams['figure.figsize'] = (40.0, 40.0)  # set default size of plots
-    num_images = len(mislabeled_indices[0])
-    for i in range(num_images):
-        index = mislabeled_indices[1][i]
-
-        plt.subplot(2, num_images, i + 1)
-        plt.imshow(X[:, index].reshape(64, 64, 3), interpolation='nearest')
-        plt.axis('off')
-        plt.title(
-            "Prediction: " + classes[int(p[0, index])].decode("utf-8") + " \n Class: " + classes[y[0, index]].decode(
-                "utf-8"))
+# def softmax(Z):
+#     shiftZ = Z - np.max(Z)
+#     exps = np.exp(shiftZ)
+#     A = exps / np.sum(exps)
+#     assert (A.shape == Z.shape)
+#     return A
+#
+#
+# def softmax_back(dA, cache):
+#     Z = cache
+#     exps = np.exp(dA - dA.max())
+#     dZ = exps / np.sum(exps, axis=0) * (1 - exps / np.sum(exps, axis=0))
+#     assert (dZ.shape == Z.shape)
+#     return dZ
